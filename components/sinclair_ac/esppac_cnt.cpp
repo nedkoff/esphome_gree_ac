439135518b3d61e98ee0e8f3d6d0ed2848ffad1e
Piotr Rzeszut <piotrrze@gmail.com>
2025-11-23 22:35:49 +0100

    Determine issue: stop packet_send after vertical swing stopping after custom_fan_mode do not cause issues

--- components/sinclair_ac/esppac_cnt.cpp
+++ components/sinclair_ac/esppac_cnt.cpp
@@ -300,8 +300,6 @@ void SinclairACCNT::send_packet()
         fanTurbo  = false;
     }
 
-    return;
-
     packet[protocol::REPORT_FAN_SPD1_BYTE] |= (fanSpeed1 << protocol::REPORT_FAN_SPD1_POS);
     packet[protocol::REPORT_FAN_SPD2_BYTE] |= (fanSpeed2 << protocol::REPORT_FAN_SPD2_POS);
     if (fanTurbo)
@@ -368,6 +366,8 @@ void SinclairACCNT::send_packet()
         mode_vertical_swing = protocol::REPORT_VSWING_OFF;
     }
     packet[protocol::REPORT_VSWING_BYTE] |= (mode_vertical_swing << protocol::REPORT_VSWING_POS);
+
+    return;
 
     /* HORIZONTAL SWING --------------------------------------------------------------------------- */
     uint8_t mode_horizontal_swing = protocol::REPORT_HSWING_OFF;
