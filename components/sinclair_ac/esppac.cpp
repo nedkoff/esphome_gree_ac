024ace14bf0496fcc6e10c84a7c8393aa9bd4eb9
nedkoff <36611026+nedkoff@users.noreply.github.com>
2026-01-14 21:47:52 +0200

    Update esppac.cpp

--- components/sinclair_ac/esppac.cpp
+++ components/sinclair_ac/esppac.cpp
@@ -130,7 +130,7 @@ void SinclairAC::update_target_temperature(float temperature)
 }
 
 
-void SinclairAC::update_vertical(const std::string &swing)
+void SinclairAC::update_swing_vertical(const std::string &swing)
 {
     this->vertical_swing_state_ = swing;
 
