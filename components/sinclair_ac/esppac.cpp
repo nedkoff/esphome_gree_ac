cd4183b2fbc1c9012ebebd4e2f542d581418afcd
nedkoff <36611026+nedkoff@users.noreply.github.com>
2026-01-22 22:01:55 +0200

    Update esppac.cpp

--- components/sinclair_ac/esppac.cpp
+++ components/sinclair_ac/esppac.cpp
@@ -273,8 +273,12 @@ void SinclairAC::set_display_select(select::Select *display_select)
 void SinclairAC::set_display_unit_select(select::Select *display_unit_select)
 {
     this->display_unit_select_ = display_unit_select;
-    this->display_unit_select_->add_on_state_callback([this](const std::string &value, size_t index) {
-        if (value == this->display_unit_state_)
+    this->on_display_unit_change_->add_on_state_callback([this](size_t index) {
+    auto opt = this->on_display_unit_change_->at(index);
+        if (!opt.has_value())
+            return;
+        const auto &value = opt.value();
+        if (value == this->on_display_unit_change_)
             return;
         this->on_display_unit_change(value);
     });
