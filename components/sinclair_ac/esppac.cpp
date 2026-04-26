aa9e3d769887a271fc47d1889958ebc5161bf646
nedkoff <36611026+nedkoff@users.noreply.github.com>
2026-04-26 10:33:05 +0300

    Moved set_supported_custom_fan_modes() from traits() to setup() to match the new ESPHome climate API.

--- components/sinclair_ac/esppac.cpp
+++ components/sinclair_ac/esppac.cpp
@@ -20,8 +20,8 @@ climate::ClimateTraits SinclairAC::traits()
     traits.set_supported_modes({climate::CLIMATE_MODE_OFF, climate::CLIMATE_MODE_AUTO, climate::CLIMATE_MODE_COOL,
                                 climate::CLIMATE_MODE_HEAT, climate::CLIMATE_MODE_FAN_ONLY, climate::CLIMATE_MODE_DRY});
 
-    traits.set_supported_custom_fan_modes({fan_modes::FAN_AUTO, fan_modes::FAN_LOW, fan_modes::FAN_MED,
-                                                           fan_modes::FAN_HIGH, fan_modes::FAN_TURBO});
+   // traits.set_supported_custom_fan_modes({fan_modes::FAN_AUTO, fan_modes::FAN_LOW, fan_modes::FAN_MED,
+   //                                                        fan_modes::FAN_HIGH, fan_modes::FAN_TURBO});
 
     traits.set_supported_swing_modes({climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_VERTICAL});
 
@@ -30,6 +30,13 @@ climate::ClimateTraits SinclairAC::traits()
 
 void SinclairAC::setup()
 {
+     this->set_supported_custom_fan_modes({
+        fan_modes::FAN_AUTO,
+        fan_modes::FAN_LOW,
+        fan_modes::FAN_MED,
+        fan_modes::FAN_HIGH,
+        fan_modes::FAN_TURBO
+     });
   // Initialize times
     this->init_time_ = millis();
     this->last_packet_sent_ = millis();
